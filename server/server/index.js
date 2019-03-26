281e645f2f28a08be2d5938f61af5e6e5771d77d
Julia <Julia@Julias-MacBook-Pro.local>
2019-03-26 15:26:01 -0400

    Connected to heroku

--- server/server/index.js
+++ server/server/index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send({ hello: "there" });
 });
 
 app.listen(PORT, () => {
