7c6ad4853ab4654741dcf847eab2b0f2d0f4ab2c
Julia <Julia@Julias-MacBook-Pro.local>
2019-03-27 16:55:14 -0400

    Linked passport to mongodb database. Added cookie session support for user authentication.

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,4 +4,14 @@ module.exports = app => {
   app.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.session);
+    // res.send(req.user);
+  });
 };
