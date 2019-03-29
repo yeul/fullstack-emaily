f95d09566eee3f69addebc791649418c1816bce0
Julia <Julia@Julias-MacBook-Pro.local>
2019-03-29 12:02:09 -0400

    Set up basics of App component and reducer with Redux. Created dummy routes for React-Router.

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,8 +1,26 @@
 import React from "react";
-import { Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>Hello, testing, 1, 2, 3</div>;
+  return (
+    <div>
+      <Router>
+        <div>
+          <Header />
+          <Switch>
+            <Route path='/' exact component={Landing} />
+            <Route path='/dashboard' exact component={Dashboard} />
+            <Route path='/surveys/new' exact component={SurveyNew} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
