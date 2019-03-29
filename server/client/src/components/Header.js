e42ff97e53b12ffd20b1b947760cf41274b7fa16
Julia <Julia@Julias-MacBook-Pro.local>
2019-03-29 16:50:32 -0400

    Login/Logout buttons now work as they should with redirects

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,15 +1,34 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchUser } from "../actions";
 
 class Header extends React.Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href='/auth/google'>Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href='/api/logout'>Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className='nav-wrapper'>
           <a className='brand-logo left'>Emaily</a>
           <ul id='nav-mobile' className='right'>
-            <li>
-              <a href='sass.html'>Login With Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -17,4 +36,11 @@ class Header extends React.Component {
   }
 }
 
-export default Header;
+const mapStateToProps = state => {
+  return { auth: state.auth };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchUser }
+)(Header);
