1b205ed4df736d237b2dd758c0690629ec007df1
Julia <Julia@Julias-MacBook-Pro.local>
2019-04-01 17:11:33 -0400

    Created email template and mailer for sendgrid.

---
+++ server/services/emailTemplates/surveyTemplate.js
@@ -0,0 +1,19 @@
+const keys = require("../../config/keys");
+
+module.exports = survey => {
+  return `
+  <html>
+    <body>
+      <div style="text-align: center;">
+        <h3>We'd like your input!</h3>
+        <p>Please answer the following question:</p>
+        <p>${survey.body}</p>
+        <div>
+          <a href="http://localhost:3000">Yes</a>
+          <a href="http://localhost:3000">No</a>
+        </div>
+      </div>
+    </body>
+  </html>
+  `;
+};
