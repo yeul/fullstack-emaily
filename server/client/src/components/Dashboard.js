34d904745a12273e5a3cb03c1f1534da35ebfaba
Julia <Julia@Julias-MacBook-Pro.local>
2019-04-05 14:02:18 -0400

    Wired up SurveyList component to Redux and added some styling.

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SurveyList from "./surveys/surveyList";
 
 const Dashboard = () => {
   return (
-    <div class='fixed-action-btn'>
-      <Link to='/surveys/new' class='btn-floating btn-large red accent-3'>
-        <i class='large material-icons'>add</i>
-      </Link>
+    <div>
+      <SurveyList />
+      <div class='fixed-action-btn'>
+        <Link to='/surveys/new' class='btn-floating btn-large red accent-3'>
+          <i class='large material-icons'>add</i>
+        </Link>
+      </div>
     </div>
   );
 };
