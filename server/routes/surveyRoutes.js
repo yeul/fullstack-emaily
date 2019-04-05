89d1b93280ac2318a378369990100d6253fedd7c
Julia <Julia@Julias-MacBook-Pro.local>
2019-04-05 13:35:40 -0400

    Created new route handler to fetch all surveys in database

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -10,7 +10,13 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("Survey");
 
 module.exports = app => {
-  app.get("/api/surveys/thanks", (req, res) => {
+  app.get("/api/surveys", requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({ recipients: false });
+
+    res.send(surveys);
+  });
+
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
     res.send("Thank you for your feedback!");
   });
 
@@ -36,7 +42,8 @@ module.exports = app => {
           },
           {
             $inc: { [choice]: 1 },
-            $set: { "recipients.$.responded": true }
+            $set: { "recipients.$.responded": true },
+            lastResponded: new Date()
           }
         ).exec();
       })
